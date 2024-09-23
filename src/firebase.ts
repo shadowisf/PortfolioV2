fc3d08369a0de6470d20cdefd3ce55f4d3d8b766
shadowisf <lespaul021504@gmail.com>
2024-09-23 18:07:13 +0400

    updated

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP0BGDaRlHLH39n8ib7HUqfweEGt9yzZ8",
-  authDomain: "les-ranalan.firebaseapp.com",
-  databaseURL:
-    "https://les-ranalan-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "les-ranalan",
-  storageBucket: "les-ranalan.appspot.com",
-  messagingSenderId: "729081233246",
-  appId: "1:729081233246:web:615c884aee752f8fb5d9cf",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
+export const firebaseApp = initializeApp(firebaseConfig);
